ddda8add2e5027eee52cd5a67c774dda09e90256
AbhishekMandapmalvi <125425135+AbhishekMandapmalvi@users.noreply.github.com>
2025-02-10 19:54:26 -0500

    Update vehicle_status.py

--- data_cleaning/vehicle_status.py
+++ data_cleaning/vehicle_status.py
@@ -3,8 +3,11 @@ from pyspark.sql import functions as F
 def vehicle_status(df):
     df = df.withColumn(
         "vehicle_status",
-        F.when((df.speed_kmh > 0) | (df.battery_current < 0), "driving")
-         .when((df.speed_kmh == 0) & (df.battery_current > 0), "charging")
-         .otherwise("idle")
+        F.when((df.speed_kmh > 1) & (df.battery_current < 0), "driving")
+         .when((df.speed_kmh > 1) & (df.battery_current > 0), "recuperation")
+         .when((df.speed_kmh <= -1) & (df.battery_current < 0), "fault")
+         .when(((df.speed_kmh < 1) & (df.speed_kmh > -1)) & (df.battery_current > 0), "charging")
+         .when(((df.speed_kmh < 1) & (df.speed_kmh > -1)) & (df.battery_current == 0), "parking")
+         .otherwise("unknown")
     )
     return df
